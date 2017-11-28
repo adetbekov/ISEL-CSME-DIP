cbbce5e4fb1a1bd1002a853247f7fa3cd7df4b8f
adetbekov <dosya@inbox.ru>
2017-11-28 17:41:00 +0000

    Add hierarchy and mask

--- LAB_1_Coins/Lab_1.py
+++ LAB_1_Coins/Lab_1.py
@@ -1,14 +1,19 @@
+"""Lab_1.py: Coin Detection and Counting."""
+__author__ = "Yeldos Adetbekov"
+__email__ = "dosya@inbox.ru"
+
 import numpy as np
 import cv2, os
 
-path = 'assets/'
+path = "assets/"
 files = []
 for f in os.listdir(path):
     files.append(cv2.imread(path + f))
 imgs = np.array(files)
 
-areas = [8100, 11000, 12000, 14000, 16000, 18000, 19000]
-denominations = [0.01, 0.02, 0.10, 0.05, 0.20, 1, 0.50]
+areas = [7000, 10000, 11000, 13000, 15000, 17000, 18000, 20000]
+denominations = [0.01, 0.02, 0.10, 0.05, 0.20, 1, 0.50, 2]
+
 
 def get_nominal(n):
     n = float(n)
@@ -19,41 +24,60 @@ def get_nominal(n):
             return "{} euros and {} cents".format(int(n), "{:0.2f}".format(np.round(n-int(n), decimals=2))[2:])
     else:
         return "{} cents".format("{:0.2f}".format(np.round(n-int(n), decimals=2))[2:])
+    
+
+def disk(r):
+    y, x = np.ogrid[-r : r+1, -r : r+1]
+    return 1 * ( x ** 2 + y ** 2 <= r ** 2 )
+
 
 for img in imgs:
     total = 0
-    b,g,r = cv2.split(img)
+    b, g, r = cv2.split(img)
     retval, threshold = cv2.threshold(r, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    kernel = np.ones((10,10),np.uint8)
-    erosion = cv2.erode(threshold, kernel, iterations = 1)
-    im2, contours, hierarchy = cv2.findContours(erosion, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    for c in contours:
+
+    floodfill = threshold.copy()
+    h, w = threshold.shape[:2]
+    mask = np.zeros((h + 2, w + 2), np.uint8)
+    cv2.floodFill(floodfill, mask, (0, 0), 255)
+    floodfill_inverted = cv2.bitwise_not(floodfill)
+    cleaned_holes = cv2.morphologyEx(floodfill, cv2.MORPH_CLOSE, disk(3))
+    out = cv2.bitwise_not(( threshold | floodfill_inverted ) + cleaned_holes)
+
+    erosion = cv2.erode(out, disk(8), iterations = 2)
+    dilation = cv2.dilate(erosion, disk(4), iterations = 2)
+
+    im2, contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    
+    cleaned_contours = []
+    for (i, c) in enumerate(contours):
+        if(hierarchy[0][i][2] == -1 and hierarchy[0][i][3] == -1):
+            cleaned_contours.append(c)
+    
+    for c in cleaned_contours:
         m = cv2.moments(c)
-        (x,y),radius = cv2.minEnclosingCircle(c)
-        center = (int(x),int(y))
+        (x, y), radius = cv2.minEnclosingCircle(c)
+        center = (int(x), int(y))
         radius = int(radius)
         perimeter = cv2.arcLength(c, True)
-        length = (np.abs(perimeter)**2) / m['m00']
+        circularity = (np.abs(perimeter) ** 2) / (m['m00'] or 1)
         m = cv2.moments(c)
         s = cv2.contourArea(c)
         top = (int(x) - int(radius), int(y) - int(radius) - 7)
 
-        if(m['m00'] <= 2*np.pi*np.square(61) and m['m00'] >= 3322 and length >= 13.5 and length <= 18):
+        if(circularity >= 13.5 and circularity <= 18):
             for i, a in enumerate(areas):
-                nextElement = areas[i+1] if areas[-1] != a else 21000
+                nextElement = areas[i+1] if areas[-1] != a else 22000
                 thisElement = a
                 currentDenominations = denominations[i]
 
                 if s >= thisElement and s <= nextElement:
-                    if s >= 19100 and s <= 19500:
-                        break
-                    else:
-                        total += currentDenominations
+                    total += currentDenominations
                     cv2.circle(img, center, radius, (0, 255, 0), 3)
                     cv2.putText(img, get_nominal(currentDenominations), top, cv2.FONT_HERSHEY_COMPLEX, 0.7, (255, 255, 255), 2)
 
     cv2.putText(img, "Total: " + get_nominal(total), (50, 60), cv2.FONT_HERSHEY_COMPLEX, 1.2, (255, 255, 255), 2)
     cv2.imshow("Coin detection", img)
-    key = cv2.waitKey(0)
+    cv2.waitKey(0)
 cv2.destroyAllWindows()
 
